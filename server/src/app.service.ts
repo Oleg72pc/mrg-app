219a5d99e1f639a1752879d7769e4283296c577a
Oleg Azarov <azarov@cgeng.pro>
2025-05-24 22:02:11 +0300

    unnecessary removed

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Тест';
+    return 'Hello World! РўР•РЎРў!';
   }
 }
